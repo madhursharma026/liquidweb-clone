3ca13aff5fe0d4ebdf0525e5c06794da74dd1f9e
Madhur Sharma <105696243+madhursharma026@users.noreply.github.com>
2025-02-25 19:07:47 +0530

    Changes Done!

---
+++ pages/hosting.js
@@ -0,0 +1,21 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import Section1 from '@/components/Hosting/Section1'
+import Section2 from '@/components/Hosting/Section2'
+import Section3 from '@/components/Hosting/Section3'
+import Section4 from '@/components/Hosting/Section4'
+import Section5 from '@/components/Hosting/Section5'
+
+export default function Hosting() {
+  return (
+    <>
+      <Header />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Footer />
+    </>
+  )
+}
