84ef655c3d1046da625241dbdae16a8cde7dbe23
Madhur Sharma <105696243+madhursharma026@users.noreply.github.com>
2025-03-08 15:09:09 +0530

    Support Page Created!

---
+++ pages/support.js
@@ -0,0 +1,25 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import Section1 from '@/components/Support/Section1'
+import Section2 from '@/components/Support/Section2'
+import Section3 from '@/components/Support/Section3'
+import Section4 from '@/components/Support/Section4'
+import Section5 from '@/components/Support/Section5'
+import Section6 from '@/components/Support/Section6'
+import Section7 from '@/components/Support/Section7'
+
+export default function Support() {
+  return (
+    <>
+      <Header />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Section6 />
+      <Section7 />
+      <Footer />
+    </>
+  )
+}
